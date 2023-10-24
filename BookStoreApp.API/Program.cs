c03b112c86411ef78b79b1e288143d16235e5a35
Iggy988 <igor.rodic1988@gmail.com>
2023-10-24 22:15:55 +0200

    Add project files.

---
+++ BookStoreApp.API/Program.cs
@@ -0,0 +1,41 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, logingConfig) =>
+{
+    logingConfig.WriteTo.Console().ReadFrom.Configuration(context.Configuration);
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowAll", b =>
+    {
+        b.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
