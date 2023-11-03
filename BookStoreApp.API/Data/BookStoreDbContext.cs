5ac259a108c60154cd5122de78b7b05f3a26b060
Iggy988 <igor.rodic1988@gmail.com>
2023-11-03 22:45:08 +0100

    Seed default users to DB

--- BookStoreApp.API/Data/BookStoreDbContext.cs
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,7 +24,7 @@ public partial class BookStoreDbContext : IdentityDbContext<ApiUser>
    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //trebamo dodati 
+        //trebamo dodati da nema errora
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Author>(entity =>
@@ -53,6 +54,63 @@ public partial class BookStoreDbContext : IdentityDbContext<ApiUser>
                 .HasForeignKey(d => d.AuthorId)
                 .HasConstraintName("FK_Books_ToTable");
         });
+
+        // seed roles in EF
+        modelBuilder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER",
+                Id = "4485D0FC-B959-4718-9653-D10AF39F0E64"
+            },
+            new IdentityRole
+            {
+                Name = "Administartor",
+                NormalizedName = "ADMINISTRATOR",
+                Id = "BEAED54E-B72B-4EF6-BF58-8D9DC5CA51C5"
+            });
+
+        var hasher = new PasswordHasher<ApiUser>();
+
+        modelBuilder.Entity<ApiUser>().HasData(
+            new ApiUser
+            {
+                Id = "792352C0-90B0-43E9-93C0-7B73E22422C5",
+                Email = "admin@bookstore.com",
+                NormalizedEmail = "ADMIN@BOOKSTORE.COM",
+                UserName = "admin@bookstore.com",
+                NormalizedUserName = "ADMIN@BOOKSTORE.COM",
+                FirstName = "System",
+                LastName = "Admin",
+                PasswordHash = hasher.HashPassword(null, "P@ssword1")
+            },
+            new ApiUser
+            {
+                Id = "06650E98-2BE4-4D25-9EFD-EBF1BEDC7863",
+                Email = "user@bookstore.com",
+                NormalizedEmail = "USER@BOOKSTORE.COM",
+                UserName = "user@bookstore.com",
+                NormalizedUserName = "user@BOOKSTORE.COM",
+                FirstName = "System",
+                LastName = "User",
+                PasswordHash = hasher.HashPassword(null, "P@ssword1")
+            });
+
+        //spajamo IdentityRole sa ApiUser
+        modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+            new IdentityUserRole<string>
+            {
+                // users id
+                RoleId = "4485D0FC-B959-4718-9653-D10AF39F0E64",
+                UserId = "06650E98-2BE4-4D25-9EFD-EBF1BEDC7863"
+            },
+            new IdentityUserRole<string>
+            {
+                // admin id
+                RoleId = "BEAED54E-B72B-4EF6-BF58-8D9DC5CA51C5",
+                UserId = "792352C0-90B0-43E9-93C0-7B73E22422C5"
+            });
+        
 
         OnModelCreatingPartial(modelBuilder);
     }
---
+++ BookStoreApp.API/Migrations/20231103214157_SeedDefaultUserAndRoles.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookStoreApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultUserAndRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4485D0FC-B959-4718-9653-D10AF39F0E64", null, "User", "USER" },
+                    { "BEAED54E-B72B-4EF6-BF58-8D9DC5CA51C5", null, "Administartor", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "06650E98-2BE4-4D25-9EFD-EBF1BEDC7863", 0, "fc73d5cb-8390-47ab-a813-b46e9da5238a", "user@bookstore.com", false, "System", "User", false, null, "USER@BOOKSTORE.COM", "user@BOOKSTORE.COM", "AQAAAAIAAYagAAAAENm8Rg+/Gfw3q2CrLUrTj3WsxKAcBQPRmlvNrdNYul1quFoy2mqJL6vURAq+xOEcSw==", null, false, "a6f91290-9be6-4bde-9645-7bfd6fa7ff09", false, "user@bookstore.com" },
+                    { "792352C0-90B0-43E9-93C0-7B73E22422C5", 0, "9adad040-0763-4306-8c09-eb2ff9fa8540", "admin@bookstore.com", false, "System", "Admin", false, null, "ADMIN@BOOKSTORE.COM", "ADMIN@BOOKSTORE.COM", "AQAAAAIAAYagAAAAEJH76HjIiR3aYQxuvULMTR091O2wl07xF0xqktElspX924DchbPy8AVsEUsagsv0UQ==", null, false, "c587405e-daea-451b-b7fa-3042c7f55ffb", false, "admin@bookstore.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { "4485D0FC-B959-4718-9653-D10AF39F0E64", "06650E98-2BE4-4D25-9EFD-EBF1BEDC7863" },
+                    { "BEAED54E-B72B-4EF6-BF58-8D9DC5CA51C5", "792352C0-90B0-43E9-93C0-7B73E22422C5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "4485D0FC-B959-4718-9653-D10AF39F0E64", "06650E98-2BE4-4D25-9EFD-EBF1BEDC7863" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "BEAED54E-B72B-4EF6-BF58-8D9DC5CA51C5", "792352C0-90B0-43E9-93C0-7B73E22422C5" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4485D0FC-B959-4718-9653-D10AF39F0E64");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "BEAED54E-B72B-4EF6-BF58-8D9DC5CA51C5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "06650E98-2BE4-4D25-9EFD-EBF1BEDC7863");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "792352C0-90B0-43E9-93C0-7B73E22422C5");
+        }
+    }
+}
