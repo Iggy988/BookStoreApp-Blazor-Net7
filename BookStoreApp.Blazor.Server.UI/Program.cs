99a85d125a6eda3cd81691f38a3735ecafa8da62
Iggy988 <igor.rodic1988@gmail.com>
2023-11-11 18:54:48 +0100

    NswagStudio

--- BookStoreApp.Blazor.Server.UI/Program.cs
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -1,4 +1,5 @@
 using BookStoreApp.Blazor.Server.UI.Data;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:7049")); ;
 
 var app = builder.Build();
 
