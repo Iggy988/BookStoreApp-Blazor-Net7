b42fa8c7c109a1b5b359965eb2233e4fc0899e35
Iggy988 <igor.rodic1988@gmail.com>
2023-12-09 11:02:33 +0100

    Running WSAM and Server apps without errors

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -18,7 +17,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https:/
 
 builder.Services.AddBlazoredLocalStorage();
 
-//builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:7049"));
+
 builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 //kad god injected AuthenticationStateProvider -> moze takodje biti implemented kao custom ApiAuthenticationStateProvider
 builder.Services.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<ApiAuthenticationStateProvider>());
@@ -32,7 +31,5 @@ builder.Services.AddScoped<IBookService, BookService>();
 
 // add AutoMapper to Services container for DI
 builder.Services.AddAutoMapper(typeof(MapperConfig));
-
-
 
 await builder.Build().RunAsync();
