069789541bae739dc1a175a2be6515ef6579c1a0
Iggy988 <igor.rodic1988@gmail.com>
2023-12-10 13:58:16 +0100

    generic repository interface and class

---
+++ BookStoreApp.API/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using BookStoreApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.API.Repositories;
+
+public class GenericRepository<T> : IGenerecicRepositoriy<T> where T : class
+{
+    private readonly BookStoreDbContext _context;
+
+    public GenericRepository(BookStoreDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _context.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await GetAsync(id);
+        _context.Set<T>().Remove(entity); 
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> GetAsync(int id)
+    {
+        if (id == null)
+        {
+            return null;
+        }
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Update(entity);
+        _context.SaveChanges();
+    }
+}
---
+++ BookStoreApp.API/Repositories/IGenerecicRepositoriy.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreApp.API.Repositories;
+
+public interface IGenerecicRepositoriy<T> where T : class
+{
+    Task<T> GetAsync(int id);
+    Task<List<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
